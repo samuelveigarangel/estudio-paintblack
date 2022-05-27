6aedb3f98cd4e8a07c712cae8d2278c5ab1fafa8
Samuel V <samuelveiga@edu.unifor.br>
2022-05-27 19:56:44 -0300

    name staticfiles

--- setup/urls.py
+++ setup/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import django.contrib.auth.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
