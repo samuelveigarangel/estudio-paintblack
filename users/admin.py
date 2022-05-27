5e5dc2d101ce25a3a4a2f4cc090dd154ba4b5211
Samuel V <samuelveiga@edu.unifor.br>
2022-05-27 03:27:53 -0300

    user pesonalizado

---
+++ users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = User
+
