313f91626b8faa11906087bd00fb62f1c9e778f5
Samuel V <samuelveiga@edu.unifor.br>
2022-05-27 18:25:26 -0300

    staticfiles add

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -133,7 +133,7 @@ if DEBUG:
    os.path.join(BASE_DIR, 'static'),
    ]
 else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+   STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
