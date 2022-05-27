3153717c6a9b887e91a15da93158f4eba52895e0
Samuel V <samuelveiga@edu.unifor.br>
2022-05-27 18:42:43 -0300

    base config

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -127,13 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
--- staticfiles/js/total_mes.js
+++
@@ -1,9 +0,0 @@
-function total_valor_mes(url){
-    fetch(url, {
-        method: 'get',
-    }).then(function(result){
-        return result.json()
-    }).then(function(data){
-        document.getElementById('valor_mes').innerHTML = data.valor_mes
-    })
-}
